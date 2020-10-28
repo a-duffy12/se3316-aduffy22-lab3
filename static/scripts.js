8cdbad11a697b0c1ce8017580a1b776d6b9b7343
Aiden <12aduffy@gmail.com>
2020-10-28 15:06:08 -0400

    [Feat] Front end q6 and fixes for q3
    + Implemented back end functionality 6 into the front end
    ~ Reworked functionality 3 into a single function, with a single button

--- static/scripts.js
+++ static/scripts.js
@@ -4,8 +4,7 @@
 let output = document.getElementById("output"); // field to print output to
 let buttonQ1 = document.getElementById("buttonQ1");
 let buttonQ2 = document.getElementById("buttonQ2");
-let buttonQ3a = document.getElementById("buttonQ3a");
-let buttonQ3b = document.getElementById("buttonQ3b");
+let buttonQ3 = document.getElementById("buttonQ3")
 let buttonQ4 = document.getElementById("buttonQ4");
 let buttonQ5 = document.getElementById("buttonQ5");
 let buttonQ6 = document.getElementById("buttonQ6");
@@ -16,11 +15,10 @@ let buttonQ9 = document.getElementById("buttonQ9");
 // event handlers for each function
 buttonQ1.addEventListener("click", displayCourses);
 buttonQ2.addEventListener("click", displayCourseCodes);
-buttonQ3a.addEventListener("click", displayTimeTableFull);
-buttonQ3b.addEventListener("click", displayTimeTableMini);
+buttonQ3.addEventListener("click", displayTimeTable);
 
 
-
+buttonQ6.addEventListener("click", displaySchedule);
 buttonQ7.addEventListener("click", deleteSchedule);
 
 buttonQ9.addEventListener("click", deleteAllSchedules);
@@ -77,99 +75,100 @@ function displayCourseCodes()
     }   
 }
 
-// function to get a timetable entry(ies) when given a subject/catalog q3
-function displayTimeTableFull()
+// function to get a timetable entry(ies) when given a subject/catalog/(component) q3
+function displayTimeTable()
 {
     let subject = prompt("Please enter a subject code: ");
     let catalog = prompt("Please enter a catalog number: ");
+    let longV = prompt("If you would like to search for a specific component, type 'yes'\nIf you would like to view all components, type anything else");
 
-    if (validate(subject) && validate(catalog))
-    {
-        clear();
 
-        let data = document.createTextNode("");
-        let req = new Request("/api/courses/" + subject + "/" + catalog, {
+    if (longV.toLocaleLowerCase() != "yes")
+    {
+            if (validate(subject) && validate(catalog))
+        {
+            clear();
 
-            method: "GET",
-            headers: new Headers ({
-                "Content-Type": "application/json"
-            })
-        });
+            let data = document.createTextNode("");
+            let req = new Request("/api/courses/" + subject + "/" + catalog, {
 
-        fetch(req)
-            .then(res => res.text())
-            .then(cons => data.textContent = cons)
-            .then(output.appendChild(data))
-            .catch(error => console.error("Error: " + error));
-    } 
-    else if (validate(subject))
-    {
-        alert("Invalid input for field(s): catalog number!");
-    }
-    else if (validate(catalog))
-    {
-        alert("Invalid input for field(s): subject code!");
-    }
-    else
-    {
-        alert("Invalid input for field(s): subject code, catalog number");
-    }
-}
+                method: "GET",
+                headers: new Headers ({
+                    "Content-Type": "application/json"
+                })
+            });
 
-// function to get a timetable entry(ies) when given a subject/catalog/component) q3
-function displayTimeTableMini()
-{
-    let subject = prompt("Please enter a subject code: ");
-    let catalog = prompt("Please enter a catalog number: ");
-    let component = prompt("Please enter a course component: ");
+            fetch(req)
+                .then(res => res.text())
+                .then(cons => data.textContent = cons)
+                .then(output.appendChild(data))
+                .catch(error => console.error("Error: " + error));
+        } 
+        else if (validate(subject))
+        {
+            alert("Invalid input for field(s): catalog number!");
+        }
+        else if (validate(catalog))
+        {
+            alert("Invalid input for field(s): subject code!");
+        }
+        else
+        {
+            alert("Invalid input for field(s): subject code, catalog number");
+        }
+    }
+    else if (longV.toLowerCase() == "yes")
+    {
+        let component = prompt("Please enter a course component: ");
 
-    if (validate(subject) && validate(catalog) && validate(component))
-    {
-        clear();
+        if (validate(subject) && validate(catalog) && validate(component))
+        {
+            clear();
 
-        let data = document.createTextNode("");
-        let req = new Request("/api/courses/" + subject + "/" + catalog + "/" + component, {
+            let data = document.createTextNode("");
+            let req = new Request("/api/courses/" + subject + "/" + catalog + "/" + component, {
 
-            method: "GET",
-            headers: new Headers ({
-                "Content-Type": "application/json"
-            })
-        });
+                method: "GET",
+                headers: new Headers ({
+                    "Content-Type": "application/json"
+                })
+            });
 
-        fetch(req)
-            .then(res => res.text())
-            .then(cons => data.textContent = cons)
-            .then(output.appendChild(data))
-            .catch(error => console.error("Error: " + error));
-    } 
-    else if (validate(subject) && validate(component))
-    {
-        alert("Invalid input for field(s): catalog number!");
-    }
-    else if (validate(catalog) && validate(component))
-    {
-        alert("Invalid input for field(s): subject code!");
-    }
-    else if (validate(subject) && validate(catalog))
-    {
-        alert("Invalid input for field(s): component!");
-    }
-    else if (validate(component))
-    {
-        alert("Invalid input for field(s): subject code, catalog number");
-    }
-    else if (validate(subject))
-    {
-        alert("Invalid input for field(s): catalog number, component");
-    }
-    else if (validate(catalog))
-    {
-        alert("Invalid input for field(s): subject code, component");
-    }
-    else
-    {
-        alert("Invalid input for field(s): subject code, catalog number, component");
-    }
+            fetch(req)
+                .then(res => res.text())
+                .then(cons => data.textContent = cons)
+                .then(output.appendChild(data))
+                .catch(error => console.error("Error: " + error));
+        } 
+        else if (validate(subject) && validate(component))
+        {
+            alert("Invalid input for field(s): catalog number!");
+        }
+        else if (validate(catalog) && validate(component))
+        {
+            alert("Invalid input for field(s): subject code!");
+        }
+        else if (validate(subject) && validate(catalog))
+        {
+            alert("Invalid input for field(s): component!");
+        }
+        else if (validate(component))
+        {
+            alert("Invalid input for field(s): subject code, catalog number");
+        }
+        else if (validate(subject))
+        {
+            alert("Invalid input for field(s): catalog number, component");
+        }
+        else if (validate(catalog))
+        {
+            alert("Invalid input for field(s): subject code, component");
+        }
+        else
+        {
+            alert("Invalid input for field(s): subject code, catalog number, component");
+        }
+    }  
 }
 
 // function to create a new schedule with subject+catalog pairs and a given name q4
@@ -178,6 +177,34 @@ function displayTimeTableMini()
 // overwrite an existing schedule of the same name q5
 
 // function to print the subject+catalog pairs in a given schedule q6
+function displaySchedule()
+{
+    let schedule = prompt("Please enter a schedule name: ");
+
+    if (validate(schedule))
+    {
+        clear();
+
+        let data = document.createTextNode("");
+        let req = new Request("/api/schedules/" + schedule, {
+            
+            method: "GET",
+            headers: new Headers ({
+                "Content-Type": "application/json"
+            })
+        });
+
+        fetch(req)
+            .then(res => res.text())
+            .then(cons => data.textContent = cons)
+            .then(output.appendChild(data))
+            .catch(error => console.error("Error: " + error));
+    }
+    else 
+    {
+        alert("Invalid input!");
+    }
+}
 
 // function to delete a schedule with a given name q7
 function deleteSchedule() 
@@ -235,7 +262,7 @@ function deleteAllSchedules()
 // function to sanitize alphanumeric input on the front end
 function validate(input)
 {
-    if (input.includes("{") || input.includes("}") || input.includes("[") || input.includes("]") || input.includes("<") || input.includes(">") ||input.includes(";") || input.includes(".") || input.includes(",") || input.includes("/") || input.includes("(") || input.includes(")") || input.includes("*"))
+    if (String(input).includes("{") || String(input).includes("}") || String(input).includes("[") || String(input).includes("]") || String(input).includes("<") || String(input).includes(">") || String(input).includes(";") || String(input).includes(".") || String(input).includes(",") || String(input).includes("/") || String(input).includes("(") || String(input).includes(")") || String(input).includes("*") || String(input).includes("*") || String(input).includes("'") || String(input).includes("_") || String(input).includes("-"))
     {
         return false;
     }
